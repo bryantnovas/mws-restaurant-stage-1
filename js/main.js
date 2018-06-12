e18125bc6ca3cba4132e119c1b86baf3a631a5a9
Bryant Novas <bryant.novas@gmail.com>
2018-06-11 21:39:15 -0400

    fixes restaurant.html

--- js/main.js
+++ js/main.js
@@ -140,13 +140,13 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = restaurant.name +' image'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
-  image.alt = restaurant.name +' image'
+  li.append(name)
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
